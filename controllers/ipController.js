6fe1ee8900b121742e2604f807a263935ff05739
senga <sunjiajia@shouqianba.com>
2024-02-26 17:19:58 +0800

    修改内容

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -4,7 +4,10 @@ exports.getCurrentIp = async (ctx) => {
         // 默认访问以太网
         const { type } = ctx.request.query || 'en0'
         const interfaces = await os.networkInterfaces();
+        // 测试
         ctx.body = {
+            type,
+            value: os.networkInterfaces(),
             data: interfaces[type]
         }
     } catch (e) {
